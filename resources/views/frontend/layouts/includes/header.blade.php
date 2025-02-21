4ca4c8c8be258aab6ea82c04178f57b1a53e859e
sayujkuickel <sayujkuickel@gmail.com>
2025-02-21 10:24:39 +0545

    header and footer

--- resources/views/frontend/layouts/includes/header.blade.php
+++ resources/views/frontend/layouts/includes/header.blade.php
@@ -1,4 +1,164 @@
-<header>
+<div class="_header">
+    <div class="container">
+        <section class="desktop_header">
+            <div class="_logo">
+                    @if (is_object($setting) && isset($setting['logo']))
+                        <a href="{{ url('/') }}">
+                            <img src="{{ asset('uploaded-images/site-setting-images/' . $setting->logo) }}"
+                                width="250">
+                        </a>
+                    @else
+                        <a href="{{ url('/') }}">
+                            <img alt="logo" class=""
+                                src="{{ asset('frontend/global-journey/assets/img/logo.png') }}" width="250">
+                        </a>
+                    @endif
+            </div>
+            <ul class="header_menu">
+                <li class='header_item'>
+                    <a href="{{ url('/') }}">Home</a>
+                </li>
+                <li class='header_item'>
+                    <a href="{{ route('about-us') }}">About Us</a>
+                </li>
+                <li class='header_item'>
+                    <a href="{{ route('study-abroad') }}">Study Abroad</a>
+                    <i class="fa-solid fa-angle-down"></i>
+                    <ul class="header_submenu">
+                        @foreach ($studyabroads as $studyabroad)
+                            <li><a
+                                    href="{{ route('study-abroad.details', $studyabroad->slug) }}">{{ $studyabroad->title }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+                <li class='header_item'>
+                    <a href="{{ route('test-preparation') }}">Test Preparation</a>
+                    <i class="fa-solid fa-angle-down"></i>
+                    <ul class="header_submenu">
+                        @foreach ($testpreparations as $testpreparation)
+                            <li>
+                                <a href="{{ route('test-preparation.details', $testpreparation->slug) }}">
+                                    {{ $testpreparation->title }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+                <li class='header_item'>
+                    <a href="{{ route('contact-us') }}">Contact Us</a>
+                </li>
+            </ul>
+        </section>
+
+        <section class="desktop_mobile">
+            <div class="_logo">
+                    @if (is_object($setting) && isset($setting['logo']))
+                        <a href="{{ url('/') }}">
+                            <img src="{{ asset('uploaded-images/site-setting-images/' . $setting->logo) }}"
+                                width="250">
+                        </a>
+                    @else
+                        <a href="{{ url('/') }}">
+                            <img alt="logo" class=""
+                                src="{{ asset('frontend/global-journey/assets/img/logo.png') }}" width="250">
+                        </a>
+                    @endif
+            </div>
+
+            <div class="_btn" id='open_main_btn'>
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </section>
+    </div>
+
+    
+</div>
+<section id='mobile_open_main' class='mobile_open_main'>
+        <div class="container">
+            <div class="close_row">
+                <div class="_logo">
+                        @if (is_object($setting) && isset($setting['logo']))
+                            <a href="{{ url('/') }}">
+                                <img src="{{ asset('uploaded-images/site-setting-images/' . $setting->logo) }}"
+                                    width="250">
+                            </a>
+                        @else
+                            <a href="{{ url('/') }}">
+                                <img alt="logo" class=""
+                                    src="{{ asset('frontend/global-journey/assets/img/logo.png') }}" width="250">
+                            </a>
+                        @endif
+                </div>
+
+                <i id='close_main_btn' class="fa-solid fa-xmark"></i>
+            </div>
+
+
+            <ul class="header_menu">
+                <li class='header_item'>
+                    <a href="{{ url('/') }}">Home</a>
+                </li>
+                <li class='header_item'>
+                    <a href="{{ route('about-us') }}">About Us</a>
+                </li>
+                <li class='header_item'>
+                    <p>
+                        <a href="{{ route('study-abroad') }}">Study Abroad</a>
+                        <i class="fa-solid fa-angle-down"></i>
+                    </p>
+
+                    <ul class="header_submenu">
+                        @foreach ($studyabroads as $studyabroad)
+                            <li><a
+                                    href="{{ route('study-abroad.details', $studyabroad->slug) }}">{{ $studyabroad->title }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+                <li class='header_item'>
+                    <p>
+                        <a href="{{ route('test-preparation') }}">Test Preparation</a>
+                        <i class="fa-solid fa-angle-down"></i>
+                    </p>
+
+                    <ul class="header_submenu">
+                        @foreach ($testpreparations as $testpreparation)
+                            <li>
+                                <a href="{{ route('test-preparation.details', $testpreparation->slug) }}">
+                                    {{ $testpreparation->title }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+                <li class='header_item'>
+                    <a href="{{ route('contact-us') }}">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    const openMenuButton = document.getElementById('open_main_btn');
+    const closeMenuButton = document.getElementById('close_main_btn');
+    const mobileMenu = document.getElementById('mobile_open_main');
+
+    // Open menu
+    openMenuButton.addEventListener('click', function () {
+        mobileMenu.style.display = 'block';
+    });
+
+    // Close menu
+    closeMenuButton.addEventListener('click', function () {
+        mobileMenu.style.display = 'none';
+    });
+});
+
+</script>
+
+<!-- <header>
     <div class="container">
         <div class="nav">
             <div class="d-flex align-items-center justify-content-between w-100">
@@ -125,47 +285,4 @@
         <a href="JavaScript:void(0)" id="res-cross"></a>
     </div>
 </header>
-
-
-
-<!-- <header>
-    <div class="container header">
-        <section class="_desktop_header"></section>
-        
-        <section class="_mobile_header">
-            <div class="header_logo">
-                @if (is_object($setting) && isset($setting['logo']))
-                    <a href="{{ url('/') }}">
-                        <img src="{{ asset('uploaded-images/site-setting-images/' . $setting->logo) }}" alt="Responsive Logo">
-                    </a>
-                @else
-                    <a href="{{ url('/') }}">
-                        <img src="{{ asset('frontend/assets/img/logo.png') }}" alt="Responsive Logo">
-                    </a>
-                @endif
-            </div>
-
-        </section>
-    </div>
-</header>
-<style>
-    header {
-        background : #d6d6d6;
-        padding-inline: 2rem; 
-        padding-block: 1rem;
-    }
-    ._mobile_header {
-        display: flex; 
-        align-items: center;
-        justify-content: space-between;
-        background: red;
-    }
-    
-    ._mobile_header img {
-        width: 100%;
-        height: 100%;
-        object-fit: contain;     
-        
-    }
-
-</style> -->
+ -->
